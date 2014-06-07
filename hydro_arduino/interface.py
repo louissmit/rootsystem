99c37a014f1594be666cb708369b1abcc37c2a06
louissmit <ghr1Co74G9>
2014-06-07 20:25:35 +0200

    first!

---
+++ hydro_arduino/interface.py
@@ -0,0 +1,18 @@
+import serial
+
+ser = serial.Serial('/dev/tty.usbmodem621', 9600)
+connected = False
+while not connected:
+	ser.read()
+	connected = True
+
+result = []
+open('results.txt', 'w').close()
+while True:
+	try:
+		with open("results.txt", "a") as file:
+			file.write(ser.readline())
+	except Exception, e:
+		print e
+		continue
+
