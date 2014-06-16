b76e87ae1cb0336d65f5fffb8d38fbf2be78a5f6
Karen Ullrich <karen.ullrich@ofai.at>
2014-06-16 23:50:21 +0200

    louis sucks

--- sensor/code/plot.py
+++ sensor/code/plot.py
@@ -2,38 +2,65 @@ import matplotlib.pyplot as plt
 from colorsys import rgb_to_hsv
 import numpy as np
 import sys
+import os
+from optparse import OptionParser
+import itertools
+
+from wav2RGB import RGB2wav
 
 ###usage##
 #python plot.py outfile
+def opts_parser():
+	usage =\
+"""\r%prog: Processing of sensor data.
 
+Usage: %prog [OPTIONS] OUTFILE
+  OUTFILE: .png file to plot the map to or .npz file to save the resluts to
+"""
+	parser = OptionParser(usage=usage)
+	parser.add_option('-s', '--save',
+			type='choice', #int,str,float
+			choices=('on', 'off'), default='on',
+			help='Results are being saved or not (default: %default)')
+
+	parser.add_option('--plotformat', metavar='format',
+			type='choice', choices=('pdf', 'png'), default='png',
+			help='Format of the plot (default: %default)')
+	
+	return parser
 
 def main():
 	# read arguments
-
+	parser = opts_parser()
+	options, args = parser.parse_args()
+	
 	#plot results
 	# Y = [int(x) for x in list(open('results.txt', 'r'))]
 	results = list(open('results.txt', 'r'))[22:]
 	wls = []
-	Y = []
-	for i in xrange(3, len(results)):
-		if ((i-1) % 2) == 0:
-			sensor = int(results[i])
-			Y.append(sensor)
-			rgb = [int(channel)/255.0 for channel in results[i-1].split()]
-			h = rgb_to_hsv(rgb[0], rgb[1], rgb[2])[0]
-			wls.append(h)
-	sortwls = np.argsort(wls)
-	Y = [Y[i] for i in sortwls][::-1]
+	intensity = []
+	num_results=len(results)/2
+
+	for i in xrange(num_results):
+		sensor = int(results[2*i])
+		intensity.append(sensor)
+		r,g,b = [int(channel)/255.0 for channel in results[2*i+1].split()]
+		wavelegth = RGB2wav(r,g,b)
+		wls.append(wavelegth)
+	
+	#sortwls = np.argsort(wls)
+	#intensity = [intensity[i] for i in sortwls][::-1]
 	# X = [380.0 + x*(750.0-380.0) for x in np.sort(wls)[::-1]]
-	X = [x for x in xrange(0, len(Y))]
-	plt.scatter(X, Y)
+	#X = [x for x in xrange(0, len(intensity))]
+	plt.scatter(wls, intensity)
 	plt.ylim(0,1100)
+	plt.xlim(300,650)
 	plt.show()
-	plt.save(outfile.png)
+	plt.save(arg[1]+options.format)
 
 	#save results to file
 	np.save(results,outfile)
 
-if __name__ is main:
 
+if __name__=="__main__":
 	main()
